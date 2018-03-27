6664204f7637dcca4d88f52adaab8e0694788907
Caxvalencia <caxvalencia@gmail.com>
2018-03-26 23:55:26 -0500

    Add methods to helper image-data

--- src/helpers/image-data.helper.ts
+++ src/helpers/image-data.helper.ts
@@ -2,6 +2,31 @@ import { CanvasHelper } from './canvas.helper';
 
 export abstract class ImageDataHelper {
     /**
+     * @description - Convierte un imageData object a un image object
+     * @param {ImageData} imageData - Objeto con matriz de datos de la imagen
+     * @return {HTMLImageElement}
+     */
+    static getImage(imageData) {
+        ImageDataHelper.validate(imageData);
+
+        let imgWidth = imageData.width;
+        let imgHeight = imageData.height;
+
+        let canvas = CanvasHelper.create(imgWidth, imgHeight);
+        let context = canvas.getContext('2d');
+
+        context.putImageData(imageData, 0, 0);
+
+        let image = new Image();
+        image.width = imgWidth;
+        image.height = imgHeight;
+
+        image.src = canvas.toDataURL('image/png');
+
+        return image;
+    }
+
+    /**
      * @static
      * @param {HTMLImageElement} image
      * @returns {ImageData}
@@ -18,9 +43,9 @@ export abstract class ImageDataHelper {
 
     /**
      * @static
-     * @param {number} width 
-     * @param {number} height 
-     * @returns {ImageData} 
+     * @param {number} width
+     * @param {number} height
+     * @returns {ImageData}
      */
     static create(width: number, height: number): ImageData {
         return CanvasHelper.context(width, height).createImageData(
@@ -28,4 +53,21 @@ export abstract class ImageDataHelper {
             height
         );
     }
+
+    /**
+     * @static
+     * @param {ImageData} imageData 
+     */
+    static validate(imageData: ImageData) {
+        if (is(imageData) !== 'imagedata') {
+            throw 'ImageData Exception: No es compatible el tipo de dato';
+        }
+    }
+}
+
+function is(element) {
+    return {}.toString
+        .call(element)
+        .match(/\s([a-z|A-Z]+)/)[1]
+        .toLowerCase();
 }
